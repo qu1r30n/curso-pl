a44155c45784b142afb13200e99ace6aebbe88d0
qu1r30n <qu1r30n@hotmail.com>
2019-05-17 15:14:58 -0500

    pagina con imagenes django
    pagina con imajenes

--- django/2/platzigram/platzigram/settings.py
+++ django/2/platzigram/platzigram/settings.py
@@ -31,12 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+#django apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+#local apps
+    'posts',
 ]
 
 MIDDLEWARE = [
