8365effd03f29657f5fb4880487c6915121e94e6
qu1r30n <qu1r30n@hotmail.com>
2019-06-08 16:43:29 -0500

    se metio en el aria de registros las partes del perfil
    semetio en el area de registros las partes del perfil para no estar llendo a la pagina de perfiles y ingresar tus datos si no ya hacerlo todo de una ves en la parte cuando te registras

--- django/prueba_mia_definitiva_desde_0/platzigram/users/admin.py
+++ django/prueba_mia_definitiva_desde_0/platzigram/users/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
+from django.contrib.auth.models import User
 from users.models import Profile
 # Register your models here.
 
@@ -47,3 +49,23 @@ class ProfileAdmin(admin.ModelAdmin):
 		)
 
 	readonly_fields = ('created','modified')
+
+class ProfileInline(admin.StackedInline):
+	model = Profile
+	can_delete = False
+	verbose_name_plural = 'profiles'
+	
+class UserAdmin(BaseUserAdmin):
+	inlines = (ProfileInline,)
+	    list_display = (
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+        'is_active',
+        'is_staff'
+    )
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
